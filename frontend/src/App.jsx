904dd2979e7ad451d83955ee16073327075735f1
Alla Magina <babara@flylady.su>
2026-02-12 16:47:04 +0300

    feat: implement Redux-based authentication system with protected routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,17 +1,37 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { Provider } from 'react-redux'
+import store from './store'
 import Home from './pages/Home'
 import Login from './pages/Login'
 import NotFound from './pages/NotFound'
+import { useEffect } from 'react'
+import { useNavigate } from 'react-router-dom'
+import { useSelector } from 'react-redux'
+
+const ProtectedRoute = ({ children }) => {
+  const navigate = useNavigate()
+  const { token } = useSelector(state => state.auth)
+
+  useEffect(() => {
+    if (!token) {
+      navigate('/login')
+    }
+  }, [token, navigate])
+
+  return token ? children : null
+}
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="*" element={<NotFound />} />
-      </Routes>
-    </Router>
+    <Provider store={store}>
+      <Router>
+        <Routes>
+          <Route path="/" element={<ProtectedRoute><Home /></ProtectedRoute>} />
+          <Route path="/login" element={<Login />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </Router>
+    </Provider>
   )
 }
 
--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,30 +1,111 @@
-import React from 'react'
 import { Formik, Form, Field, ErrorMessage } from 'formik'
+import { useNavigate, Link } from 'react-router-dom'
+import { useDispatch, useSelector } from 'react-redux'
+import { loginUser, clearError } from '../store/authSlice'
+import { useEffect } from 'react'
 
 const Login = () => {
+  const navigate = useNavigate()
+  const dispatch = useDispatch()
+  const { loading, error } = useSelector((state) => state.auth)
+
+  useEffect(() => {
+    dispatch(clearError())
+  }, [dispatch])
+
+  const validate = (values) => {
+    const errors = {}
+    if (!values.username) errors.username = 'Обязательное поле'
+    if (!values.password) errors.password = 'Обязательное поле'
+    return errors
+  }
+
+  const handleSubmit = async (values, { setSubmitting }) => {
+    dispatch(clearError())
+
+    try {
+      const resultAction = await dispatch(loginUser(values))
+      if (loginUser.fulfilled.match(resultAction)) {
+        navigate('/')
+      }
+    } finally {
+      setSubmitting(false)
+    }
+  }
+
   return (
-    <div>
-      <h1>Страница авторизации</h1>
-      <Formik
-        initialValues={{ username: '', password: '' }}
-        onSubmit={(values) => {
-          console.log('Форма отправлена:', values)
-        }}
-      >
-        <Form>
-          <div>
-            <label htmlFor="username">Имя пользователя</label>
-            <Field type="text" id="username" name="username" />
-            <ErrorMessage name="username" component="div" />
+    <div className="container-fluid h-100">
+      <div className="row justify-content-center align-content-center h-100">
+        <div className="col-12 col-md-8 col-xxl-6">
+          <div className="card shadow-sm">
+            <div className="card-body p-5">
+              <h1 className="text-center mb-4">Войти</h1>
+
+              <Formik
+                initialValues={{ username: '', password: '' }}
+                validate={validate}
+                onSubmit={handleSubmit}
+              >
+                {({ isSubmitting }) => (
+                  <Form>
+                    {error && (
+                      <div className="alert alert-danger" role="alert">
+                        {error}
+                      </div>
+                    )}
+
+                    <div className="form-floating mb-3">
+                      <Field
+                        name="username"
+                        id="username"
+                        className="form-control"
+                        placeholder="Ваш ник"
+                        autoComplete="username"
+                      />
+                      <label htmlFor="username">Ваш ник</label>
+                      <ErrorMessage
+                        name="username"
+                        component="div"
+                        className="text-danger small"
+                      />
+                    </div>
+
+                    <div className="form-floating mb-4">
+                      <Field
+                        type="password"
+                        name="password"
+                        id="password"
+                        className="form-control"
+                        placeholder="Пароль"
+                        autoComplete="current-password"
+                      />
+                      <label htmlFor="password">Пароль</label>
+                      <ErrorMessage
+                        name="password"
+                        component="div"
+                        className="text-danger small"
+                      />
+                    </div>
+
+                    <button
+                      type="submit"
+                      className="w-100 mb-3 btn btn-outline-primary"
+                      disabled={isSubmitting || loading}
+                    >
+                      {loading ? 'Вход...' : 'Войти'}
+                    </button>
+                  </Form>
+                )}
+              </Formik>
+            </div>
+
+            <div className="card-footer p-4 text-center">
+              <span>Нет аккаунта?</span>{' '}
+              <Link to="/signup">Регистрация</Link>
+            </div>
           </div>
-          <div>
-            <label htmlFor="password">Пароль</label>
-            <Field type="password" id="password" name="password" />
-            <ErrorMessage name="password" component="div" />
-          </div>
-          <button type="submit">Войти</button>
-        </Form>
-      </Formik>
+        </div>
+      </div>
     </div>
   )
 }
