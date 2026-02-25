4f74bf94e12a4c4d532009d65eae785eab634676
Alla Magina <babara@flylady.su>
2026-02-25 13:00:19 +0300

    fix login validation

--- frontend/src/validation/loginSchema.js
+++ frontend/src/validation/loginSchema.js
@@ -3,11 +3,8 @@ import * as yup from 'yup'
 export const getLoginSchema = t =>
   yup.object().shape({
     username: yup.string()
-      .min(3, t('validation.usernameRange'))
-      .max(20, t('validation.usernameRange'))
       .required(t('validation.required')),
 
     password: yup.string()
-      .min(6, t('validation.passwordMin'))
       .required(t('validation.required')),
   })
