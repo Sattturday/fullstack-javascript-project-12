68eb15ce94ec1aad5102f8679c94f9d2fa5782c0
Alla Magina <babara@flylady.su>
2026-02-25 12:52:38 +0300

    refactor Login Signup

--- frontend/src/pages/Signup.jsx
+++ frontend/src/pages/Signup.jsx
@@ -141,7 +141,7 @@ const Signup = () => {
                     <button
                       type="submit"
                       className="w-100 mb-3 btn btn-outline-primary"
-                      disabled={isSubmitting || isLoading }
+                      disabled={isSubmitting || isLoading}
                     >
                       {isLoading || isSubmitting
                         ? (
